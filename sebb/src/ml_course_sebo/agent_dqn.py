25a5c4fbfd309fa979d8197e95bad46b733cdb8a
Plasmoxy <sebohacker@gmail.com>
2019-03-20 11:05:43 +0100

    xd

--- sebb/src/ml_course_sebo/agent_dqn.py
+++ sebb/src/ml_course_sebo/agent_dqn.py
@@ -23,3 +23,45 @@ class DQNAgent(Agent):
         self.epsilon_training = epsilon_training
         self.epsilon_testing = epsilon_testing
         self.epsilon_decay = epsilon_decay
+
+    # process method for network
+    def main(self):
+
+        if self.is_run_best_enabled():
+            epsilon = self.epsilon_testing
+        else:
+            epsilon = self.epsilon_training
+            self.epsilon *= self.epsilon_decay # decay epsilon
+
+        state = self.env.get_observation()
+        state_vector = dqn.VectorFloat(self.env.get_size())
+
+        for i in range(0, state_vector.size()):
+            state_vector[i] = state[i]
+
+        # compute the q values
+        self.deep_q_network.compute_q_values(state_vector)
+        q_values = self.deep_q_network.get_q_values()
+
+        self.action = self.select_action(q_values, epsilon)
+        self.env.do_action(self.action)
+
+        # pouc sa z chyb
+        self.reward = self.env.get_reward()
+
+        # plnenie buffera
+        if self.env.is_done():
+            self.deep_q_network.add_final(state_vector, q_values, self.action, self.reward)
+        else:
+            self.deep_q_network.add(state_vector, q_values, self.action, self.reward)
+
+        # plny buffer
+        if self.deep_q_network.is_full() and not self.run_best_enabled:
+            self.deep_q_network.learn()
+
+    # citanie a ukladanie
+    def save(self, file_name_prefix):
+        self.deep_q_network.save()
+
+    def load(self, file_name):
+        self.deep_q_network.load_weights(file_name)
